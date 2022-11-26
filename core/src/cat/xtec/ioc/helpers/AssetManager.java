d7d97454d17a1b8a16c84a8e9e827a91a9c69587
isaDevs <isabel.calzadilla.18@gmail.com>
2022-11-26 11:39:09 +0100

    adhiriendo imágenes propias --> ok

--- core/src/cat/xtec/ioc/helpers/AssetManager.java
+++ core/src/cat/xtec/ioc/helpers/AssetManager.java
@@ -13,6 +13,7 @@ public class AssetManager {
 
     // Sprite Sheet
     public static Texture sheet;
+    public static Texture sheet1;
 
     // Nau i fons
     public static TextureRegion spacecraft, spacecraftDown, spacecraftUp, background;
@@ -37,26 +38,32 @@ public class AssetManager {
 
     public static void load() {
         // Carreguem les textures i li apliquem el mètode d'escalat 'nearest'
-        sheet = new Texture(Gdx.files.internal("sheet.png"));
+        //sheet = new Texture(Gdx.files.internal("sheet.png"));
+        sheet = new Texture(Gdx.files.internal("image_sheet.png"));
         sheet.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
 
         // Sprites de la nau
-        spacecraft = new TextureRegion(sheet, 0, 0, 36, 15);
+        //spacecraft = new TextureRegion(sheet, 0, 0, 36, 15);
+        spacecraft = new TextureRegion(sheet, 720, 566, 113, 120); // -- ok
         spacecraft.flip(false, true);
 
-        spacecraftUp = new TextureRegion(sheet, 36, 0, 36, 15);
+        //spacecraftUp = new TextureRegion(sheet, 36, 0, 36, 15);
+        spacecraftUp = new TextureRegion(sheet, 104, 746, 180, 180); // -- ok
         spacecraftUp.flip(false, true);
 
-        spacecraftDown = new TextureRegion(sheet, 72, 0, 36, 15);
+        //spacecraftDown = new TextureRegion(sheet, 72, 0, 36, 15);
+        spacecraftDown = new TextureRegion(sheet, 284, 746, 180, 180); // ok
         spacecraftDown.flip(false, true);
 
 
 
         // Carreguem els 16 estats de l'asteroid
+        //asteroid = new TextureRegion[16];
         asteroid = new TextureRegion[16];
         for (int i = 0; i < asteroid.length; i++) {
 
-            asteroid[i] = new TextureRegion(sheet, i * 34, 15, 34, 34);
+            //asteroid[i] = new TextureRegion(sheet, i * 34, 15, 34, 34);
+            asteroid[i] = new TextureRegion(sheet, i * 577, 788, 113, 120);
           //  asteroid[i].flip(false, true);
 
         }
@@ -74,7 +81,8 @@ public class AssetManager {
         int index = 0;
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 8; j++) {
-                explosion[index++] = new TextureRegion(sheet, j * 64,  i * 64 + 49, 64, 64);
+                //explosion[index++] = new TextureRegion(sheet, j * 64,  i * 64 + 49, 64, 64);
+                explosion[index++] = new TextureRegion(sheet, j * 0,  i * 566 + 34, 150, 150);
               //  explosion[index-1].flip(false, true);
             }
         }
@@ -83,8 +91,9 @@ public class AssetManager {
         explosionAnim = new Animation(0.04f, explosion);
 
         // Fons de pantalla
-        background = new TextureRegion(sheet, 0, 177, 480, 135);
-    //    background.flip(false, true);
+        //background = new TextureRegion(sheet1, 0, 177, 480, 135);
+        background = new TextureRegion(sheet, 0, 0, 768, 566);
+        background.flip(false, true);  // da la vuelta al fondo
 
         /******************************* Sounds *************************************/
         // Explosió
